1bca2e0161c2a06b9fb66d0732fd49a484edaead
Yoha Braga <braga.yoha@gmail.com>
2022-06-27 15:35:53 -0300

    feat: create and polished game mode buttons

--- js/script.js
+++ js/script.js
@@ -28,16 +28,50 @@
             async function loadGamesInfo () {
                 const response = await getData('../games.json')
                 gamesInfo = JSON.parse(response)
-                console.log(gamesInfo)
+                return gamesInfo
+                
             }
 
-            function createGameModeButton (gamesInfo) {
-                
+            async function insertGameModeButton () {
+                gamesInfo = await loadGamesInfo()
+                const games = gamesInfo.types
+                console.log(games)
+
+                for (let i = 0; i < games.length; i++){
+
+                    const input = doc.createElement('input')
+                    input.setAttribute('type', 'radio')
+                    input.setAttribute('class', 'btn-check')
+                    input.setAttribute('name', 'options-outlined')
+                    input.setAttribute('id', `${games[i].type}-outlined`)
+                    input.setAttribute('autocomplete', 'off')
+
+                    const label = doc.createElement('label')
+                    label.setAttribute('class', 'g-b btn btn-outline me-4')
+                    
+                    label.setAttribute('style', 
+                    `--bs-btn-color:${games[i].color};
+                    --bs-btn-border-color:${games[i].color};
+                    --bs-btn-hover-color:#FFFFFF;
+                    --bs-btn-hover-bg:${games[i].color};
+                    --bs-btn-active-color:#fff;
+                    --bs-btn-active-bg:${games[i].color};
+                    --bs-btn-active-border-color:${games[i].color};`)
+
+                    label.setAttribute('for', input.id)
+                    label.innerHTML = games[i].type
+                    doc.getElementById('game-mode-button').appendChild(input)
+                    doc.getElementById('game-mode-button').appendChild(label)
+                }
+            }
+
+            function a( ){
+
             }
 
             win.addEventListener('load', function(){
                 loadGamesInfo()
-                createGameModeButton()
+                insertGameModeButton()
             })
 
         }
