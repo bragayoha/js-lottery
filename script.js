f74af50552d24419dfd505f45cb94579946a44e6
Yoha Braga <braga.yoha@gmail.com>
2022-06-27 09:21:18 -0300

    feat: update DOM

--- script.js
+++ script.js
@@ -3,37 +3,50 @@
 
     function app () {
         
-        const ajax = new XMLHttpRequest()
-        let gamesInfo
-        let divGameMode = doc.getElementById('game-mode-button')
-        
-        function getData (endpoint) {
-            
-            ajax.responseType = 'json'
-            ajax.onreadystatechange = function () {
-                if(ajax.readyState === 4 && ajax.status === 200)
-                gamesInfo = ajax.response
-                console.log(gamesInfo)
-            }
-            ajax.open('GET', endpoint)
-            ajax.send()
-            
-        }
-        
-        function loadGamesInfo () {
-            getData('./games.json')
-        }
+        return {
+			init: function init() {
+				this.getFromServer('./games.json', this.getGamesInfo)
+				this.initEvents()
+			},
 
-        
-        function gameModeButton (data) {
-            console.log(data)
+			initEvents: function initEvents() {
+				
+			},
+
+			
+            getFromServer: function getFromServer(url, listenerCallback) {
+				const ajax = new XMLHttpRequest();
+
+				ajax.open('GET', url, true)
+				ajax.send()
+				if (listenerCallback) ajax.addEventListener('readystatechange', listenerCallback, false)
+			},
+
+            getGamesInfo: function getGamesInfo() {
+				if (this.readyState === 4 && this.status === 200) {
+					const $companyName = $('[data-js="companyName"]').get()
+					const $companyPhone = $('[data-js="companyPhone"]').get()
+					const data = JSON.parse(this.responseText)
+
+					$companyName.textContent = data.name
+					$companyPhone.textContent = data.phone
+				}
+			},
+
+            postCarsToServer: function postCarsToServer() {
+				if (this.readyState === 4 && this.status === 200)
+					app.getFromServer('http://localhost:3000/car', app.getCarsFromServer)
+			},
+
+            deleteFromServer: function deleteFromServer(url, content, listenerCallback) {
+				const ajax = new XMLHttpRequest()
+				ajax.open('DELETE', url)
+				ajax.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+				ajax.send(content)
+
+				ajax.addEventListener('readystatechange', listenerCallback, false)
+			},
         }
-        
-        win.addEventListener('load', function () {
-        loadGamesInfo()
-        gameModeButton(ajax.response)
-        })
-    }
 
     app()
-})(window.DOM, window, document)
+}})(window.DOM, window, document)
