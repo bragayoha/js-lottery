47b098a0beb8ee759d4ef15dba63517d41e9a1c8
Yoha Braga <braga.yoha@gmail.com>
2022-06-27 20:40:05 -0300

    feat: create game mode settings add game mode button dynamic

--- js/script.js
+++ js/script.js
@@ -43,7 +43,7 @@
                     input.setAttribute('type', 'radio')
                     input.setAttribute('class', 'btn-check')
                     input.setAttribute('name', 'options-outlined')
-                    input.setAttribute('id', `${games[i].type}-outlined`)
+                    input.setAttribute('id', `${games[i].type}`)
                     input.setAttribute('autocomplete', 'off')
 
                     const label = doc.createElement('label')
@@ -57,22 +57,82 @@
                     --bs-btn-active-color:#fff;
                     --bs-btn-active-bg:${games[i].color};
                     --bs-btn-active-border-color:${games[i].color};`)
-
                     label.setAttribute('for', input.id)
                     label.innerHTML = games[i].type
+
                     doc.getElementById('game-mode-button').appendChild(input)
                     doc.getElementById('game-mode-button').appendChild(label)
                 }
             }
 
-            function a( ){
+            async function firstGameSelected( ){
+                gamesInfo = await loadGamesInfo()
+                const game = gamesInfo.types[0]
+
+                const firstClick = new MouseEvent('click', {})
+                const firstGame = doc.getElementById(game.type)
+
+                firstGame.dispatchEvent(firstClick)
+            }
+
+            async function insertGameSettings (gameModeID){
+                gamesInfo = await loadGamesInfo()
+                const games = gamesInfo.types
+                const game = games.filter(x => {
+                    return x.type === gameModeID
+                })
+
+                const currentGame = game[0]
+
+                for(let i = 0; i < currentGame.range; i++){
+                    const input = doc.createElement('input')
+                    input.setAttribute('type', 'checkbox')
+                    input.setAttribute('class', 'btn-check')
+                    input.setAttribute('id', `number-${i+1}`)
+                    input.setAttribute('autocomplete', 'off')
+
+                    const label = doc.createElement('label')
+                    label.setAttribute('class', 'btn btn-gn d-flex justify-content-center align-items-center gn-space')
+                    label.setAttribute('for', input.id)
+                    label.innerHTML = i+1
+
+                    doc.getElementById('game-number-buttons').appendChild(input)
+                    doc.getElementById('game-number-buttons').appendChild(label)
+                    console.log('entrou')
+                }
+
+                const p = doc.createElement('p')
+                p.setAttribute('class', 'gd')
+                p.setAttribute('id', 'game-desc')
+
+                p.innerHTML = currentGame.description
+                doc.getElementById('gd-div').appendChild(p)
+
+            }
+
+            function clearGameSettings () {
+                // const elem = doc.getElementById('number-1')
+                // elem.parentNode.removeChild(elem)
+
+                // const p = doc.getElementById('game-desc')
+                // p.remove()
+
+            }
+
+            async function clickGameModeButton (id) {
+                clearGameSettings()
+                insertGameSettings(id)
 
             }
 
             win.addEventListener('load', function(){
                 loadGamesInfo()
                 insertGameModeButton()
+                firstGameSelected()
+                clickGameModeButton('Quina')
+
             })
+
 
         }
         app()
